5369f8754a8bb3891857b6e155f7a2cb8f49f970
Adam1416k <adamastrom02@gmail.com>
2023-02-07 11:12:28 +0100

    First Push

---
+++ Axel/Build_1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Pil_placementholder::Start()
+extern void Pil_placementholder_Start_mC0F6B40E42564A40DBAB7B44ACB45ED9AEF39E7C (void);
+// 0x00000002 System.Void Pil_placementholder::Update()
+extern void Pil_placementholder_Update_m1BD45A26DD27101F3255DB09954109AF67A8D620 (void);
+// 0x00000003 System.Void Pil_placementholder::.ctor()
+extern void Pil_placementholder__ctor_m0EE4F47B7F7412B5507CAAA33EED0683FDBCFEB1 (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	Pil_placementholder_Start_mC0F6B40E42564A40DBAB7B44ACB45ED9AEF39E7C,
+	Pil_placementholder_Update_m1BD45A26DD27101F3255DB09954109AF67A8D620,
+	Pil_placementholder__ctor_m0EE4F47B7F7412B5507CAAA33EED0683FDBCFEB1,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	2067,
+	2067,
+	2067,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
