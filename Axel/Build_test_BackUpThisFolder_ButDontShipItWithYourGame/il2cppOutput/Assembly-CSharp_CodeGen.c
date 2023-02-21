782b0388984d6e6f79e4abb345d75651399392ea
Adam1416k <adamastrom02@gmail.com>
2023-02-21 10:16:37 +0100

    Tracking

---
+++ Axel/Build_test_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Pil_placementholder::Start()
+extern void Pil_placementholder_Start_mC0F6B40E42564A40DBAB7B44ACB45ED9AEF39E7C (void);
+// 0x00000002 System.Void Pil_placementholder::Update()
+extern void Pil_placementholder_Update_m1BD45A26DD27101F3255DB09954109AF67A8D620 (void);
+// 0x00000003 System.Void Pil_placementholder::.ctor()
+extern void Pil_placementholder__ctor_m0EE4F47B7F7412B5507CAAA33EED0683FDBCFEB1 (void);
+// 0x00000004 System.Void SceneSwitcher::playGame()
+extern void SceneSwitcher_playGame_mC585D76392C889F44374FD4655532D928DEC6345 (void);
+// 0x00000005 System.Void SceneSwitcher::back()
+extern void SceneSwitcher_back_m50B9617434BB4F125DF88FC4BB78028525C38B2C (void);
+// 0x00000006 System.Void SceneSwitcher::.ctor()
+extern void SceneSwitcher__ctor_m286A74582BD4C6059DACBC69C75EE335165E73DA (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	Pil_placementholder_Start_mC0F6B40E42564A40DBAB7B44ACB45ED9AEF39E7C,
+	Pil_placementholder_Update_m1BD45A26DD27101F3255DB09954109AF67A8D620,
+	Pil_placementholder__ctor_m0EE4F47B7F7412B5507CAAA33EED0683FDBCFEB1,
+	SceneSwitcher_playGame_mC585D76392C889F44374FD4655532D928DEC6345,
+	SceneSwitcher_back_m50B9617434BB4F125DF88FC4BB78028525C38B2C,
+	SceneSwitcher__ctor_m286A74582BD4C6059DACBC69C75EE335165E73DA,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
