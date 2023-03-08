c7d0aff982b964795dbf5344ae92e3216ca1210b
Adam1416k <adamastrom02@gmail.com>
2023-03-08 11:12:53 +0100

    update

--- Axel/Assets/Timer.cs
+++ Axel/Assets/Timer.cs
@@ -4,26 +4,32 @@ using System.Collections;
 
 public class Timer : MonoBehaviour
 {
-    public float timerValue = 60f;
-    private TextMesh textMesh;
+    public float startingTime = 60f; // Starting time in seconds
+    private float currentTime; // Current time in seconds
+    private string originalText; // Original text in the TextMesh component
+    private TextMesh textMesh; // Reference to TextMesh component
 
     void Start()
     {
         textMesh = GetComponent<TextMesh>();
+        originalText = textMesh.text;
+        currentTime = PlayerPrefs.GetFloat("Timer", startingTime);
     }
 
     void Update()
     {
-        if (timerValue > 0)
+        if (currentTime > 0)
         {
-            timerValue -= Time.deltaTime;
-            int minutes = Mathf.FloorToInt(timerValue / 60f);
-            int seconds = Mathf.FloorToInt(timerValue - minutes * 60);
-            textMesh.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString("00");
+            currentTime -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(currentTime / 60f);
+            int seconds = Mathf.FloorToInt(currentTime % 60f);
+            string timerText = "Time left: " + minutes.ToString("00") + ":" + seconds.ToString("00");
+            textMesh.text = originalText + "\n" + timerText;
+            PlayerPrefs.SetFloat("Timer", currentTime);
         }
         else
         {
-            textMesh.text = "Time: 00:00";
+            textMesh.text = originalText + "\nTime's up!";
         }
     }
 }
---
+++ Axel/Build_6_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,113 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void AbortAction::Start()
+extern void AbortAction_Start_m49D5D62314EA386803B1592E1960BDDFFF014320 (void);
+// 0x00000002 System.Void AbortAction::Update()
+extern void AbortAction_Update_m700134EADCFAEB3BC80B90FC2C54E65FC7A25B9F (void);
+// 0x00000003 System.Void AbortAction::.ctor()
+extern void AbortAction__ctor_mF5FCE97AD0E65972CC5225F5781BCD0ECF65BAF0 (void);
+// 0x00000004 System.Void Pil_placementholder::Start()
+extern void Pil_placementholder_Start_mC0F6B40E42564A40DBAB7B44ACB45ED9AEF39E7C (void);
+// 0x00000005 System.Void Pil_placementholder::Update()
+extern void Pil_placementholder_Update_m1BD45A26DD27101F3255DB09954109AF67A8D620 (void);
+// 0x00000006 System.Void Pil_placementholder::.ctor()
+extern void Pil_placementholder__ctor_m0EE4F47B7F7412B5507CAAA33EED0683FDBCFEB1 (void);
+// 0x00000007 System.Void SceneSwitcher::playGame()
+extern void SceneSwitcher_playGame_mC585D76392C889F44374FD4655532D928DEC6345 (void);
+// 0x00000008 System.Void SceneSwitcher::back()
+extern void SceneSwitcher_back_m50B9617434BB4F125DF88FC4BB78028525C38B2C (void);
+// 0x00000009 System.Void SceneSwitcher::.ctor()
+extern void SceneSwitcher__ctor_m286A74582BD4C6059DACBC69C75EE335165E73DA (void);
+// 0x0000000A System.Void WaterPlant::Start()
+extern void WaterPlant_Start_mF3AD0FC2632B5003DF83748E786C5C729A8A16A1 (void);
+// 0x0000000B System.Void WaterPlant::Update()
+extern void WaterPlant_Update_m3D19CA5B3536E663F9418B2AA1ED1D8DAD1C0C4C (void);
+// 0x0000000C System.Void WaterPlant::.ctor()
+extern void WaterPlant__ctor_m068AAFB0CE90ADBF9175B4A1CAB7EFA2347B8C48 (void);
+// 0x0000000D System.Void Spider::Start()
+extern void Spider_Start_mAB2286FAFFBB830C38C76705EF4B09841C95F695 (void);
+// 0x0000000E System.Void Spider::Update()
+extern void Spider_Update_m8C4360AADE9F2B691E6B604FCA240A7D67F7A938 (void);
+// 0x0000000F System.Void Spider::.ctor()
+extern void Spider__ctor_mAAF5022CC88D8F8E23A9577EF373B8E7C28E9E9E (void);
+// 0x00000010 System.Void Timer::Start()
+extern void Timer_Start_m6820D211A06B2E5E24795D7EBE67EE865F39BC43 (void);
+// 0x00000011 System.Void Timer::Update()
+extern void Timer_Update_m3AC30EFD54E8E1010F6E601AE179212E71E6B2CD (void);
+// 0x00000012 System.Void Timer::.ctor()
+extern void Timer__ctor_m5FF13F1DAD0527F97E229A1904A8AD662731C4B5 (void);
+static Il2CppMethodPointer s_methodPointers[18] = 
+{
+	AbortAction_Start_m49D5D62314EA386803B1592E1960BDDFFF014320,
+	AbortAction_Update_m700134EADCFAEB3BC80B90FC2C54E65FC7A25B9F,
+	AbortAction__ctor_mF5FCE97AD0E65972CC5225F5781BCD0ECF65BAF0,
+	Pil_placementholder_Start_mC0F6B40E42564A40DBAB7B44ACB45ED9AEF39E7C,
+	Pil_placementholder_Update_m1BD45A26DD27101F3255DB09954109AF67A8D620,
+	Pil_placementholder__ctor_m0EE4F47B7F7412B5507CAAA33EED0683FDBCFEB1,
+	SceneSwitcher_playGame_mC585D76392C889F44374FD4655532D928DEC6345,
+	SceneSwitcher_back_m50B9617434BB4F125DF88FC4BB78028525C38B2C,
+	SceneSwitcher__ctor_m286A74582BD4C6059DACBC69C75EE335165E73DA,
+	WaterPlant_Start_mF3AD0FC2632B5003DF83748E786C5C729A8A16A1,
+	WaterPlant_Update_m3D19CA5B3536E663F9418B2AA1ED1D8DAD1C0C4C,
+	WaterPlant__ctor_m068AAFB0CE90ADBF9175B4A1CAB7EFA2347B8C48,
+	Spider_Start_mAB2286FAFFBB830C38C76705EF4B09841C95F695,
+	Spider_Update_m8C4360AADE9F2B691E6B604FCA240A7D67F7A938,
+	Spider__ctor_mAAF5022CC88D8F8E23A9577EF373B8E7C28E9E9E,
+	Timer_Start_m6820D211A06B2E5E24795D7EBE67EE865F39BC43,
+	Timer_Update_m3AC30EFD54E8E1010F6E601AE179212E71E6B2CD,
+	Timer__ctor_m5FF13F1DAD0527F97E229A1904A8AD662731C4B5,
+};
+static const int32_t s_InvokerIndices[18] = 
+{
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+	4418,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	18,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
