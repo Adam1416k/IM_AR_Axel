86ca4428461f20efaac898a2eab0e71891fae95f
fredtarn <fredrik.tarnstrom@hotmail.com>
2023-03-13 15:14:45 +0100

    nytt timer script

--- Axel/Assets/Timer.cs
+++ Axel/Assets/Timer.cs
@@ -1,37 +1,36 @@
+using System;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
 
 public class Timer : MonoBehaviour
 {
-    public float startingTime = 60f; // Starting time in seconds
-    private float currentTime; // Current time in seconds
-    private string originalText; // Original text in the TextMesh component
-    private TextMesh textMesh; // Reference to TextMesh component
+    private DateTime targetDate;
+    private TimeSpan timeRemaining;
 
     void Start()
     {
-        textMesh = GetComponent<TextMesh>();
-        originalText = textMesh.text;
-        currentTime = PlayerPrefs.GetFloat("Timer", startingTime);
+        // Load the target date from PlayerPrefs
+        string dateString = PlayerPrefs.GetString("TargetDate");
+        if (!string.IsNullOrEmpty(dateString))
+        {
+            targetDate = DateTime.Parse(dateString);
+        }
+        else
+        {
+            // Set the target date to 6 days from now
+            targetDate = DateTime.Now.AddDays(6);
+            // Save the target date to PlayerPrefs
+            PlayerPrefs.SetString("TargetDate", targetDate.ToString());
+            PlayerPrefs.Save();
+        }
+
+        // Calculate the time remaining
+        timeRemaining = targetDate - DateTime.Now;
     }
 
     void Update()
     {
-        if (currentTime > 0)
-        {
-            currentTime -= Time.deltaTime;
-            int days = Mathf.FloorToInt(currentTime / 86400f);
-            int hours = Mathf.FloorToInt((currentTime % 86400f) / 3600f);
-            int minutes = Mathf.FloorToInt((currentTime % 3600f) / 60f);
-            int seconds = Mathf.FloorToInt(currentTime % 60f);
-            string timerText = "Vattna inom: " + days.ToString("00") + ":" + hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
-            textMesh.text = originalText + "\n" + timerText;
-            PlayerPrefs.SetFloat("Timer", currentTime);
-        }
-        else
-        {
-            textMesh.text = originalText + "\nDags att vattna!";
-        }
+        // Update the time remaining every frame
+        timeRemaining = targetDate - DateTime.Now;
+        Debug.Log("Time remaining: " + timeRemaining.ToString());
     }
 }
