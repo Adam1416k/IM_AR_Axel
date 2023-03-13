e01fc6bf81d02c19de0ce283e6174f7365c59393
fredtarn <fredrik.tarnstrom@hotmail.com>
2023-03-13 16:34:09 +0100

    Timer script

--- Axel/Assets/Scripts/Timer.cs
+++ Axel/Assets/Scripts/Timer.cs
@@ -1,35 +1,54 @@
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+using System;
 
 public class Timer : MonoBehaviour
 {
-    public float startingTime = 60f; // Starting time in seconds
-    private float currentTime; // Current time in seconds
-    private string originalText; // Original text in the TextMesh component
-    private TextMesh textMesh; // Reference to TextMesh component
+    public TextMesh textMesh; // Reference to the Text Mesh component
+
+    private DateTime endDate; // Date and time when the timer should end
 
     void Start()
     {
-        textMesh = GetComponent<TextMesh>();
-        originalText = textMesh.text;
-        currentTime = PlayerPrefs.GetFloat("Timer", startingTime);
+        // Load the end date from the PlayerPrefs if it exists, otherwise set it to 3 days from now
+        if (PlayerPrefs.HasKey("endDate"))
+        {
+            string endDateString = PlayerPrefs.GetString("endDate");
+            endDate = DateTime.Parse(endDateString);
+        }
+        else
+        {
+            endDate = DateTime.Now.AddDays(3);
+            PlayerPrefs.SetString("endDate", endDate.ToString());
+        }
     }
 
     void Update()
     {
-        if (currentTime > 0)
+        // Calculate the time remaining until the end date
+        TimeSpan timeRemaining = endDate - DateTime.Now;
+
+        // If the timer has run out, set the text to indicate that
+        if (timeRemaining.TotalSeconds <= 0)
         {
-            currentTime -= Time.deltaTime;
-            int minutes = Mathf.FloorToInt(currentTime / 60f);
-            int seconds = Mathf.FloorToInt(currentTime % 60f);
-            string timerText = "Time left: " + minutes.ToString("00") + ":" + seconds.ToString("00");
-            textMesh.text = originalText + "\n" + timerText;
-            PlayerPrefs.SetFloat("Timer", currentTime);
+            textMesh.text = "Timer has run out!";
+            return;
         }
-        else
+
+        // Otherwise, format the time remaining as a string and set the text to display it
+        string timeRemainingString = string.Format("{0} days, {1:D2}:{2:D2}:{3:D2}",
+            (int)timeRemaining.TotalDays,
+            timeRemaining.Hours,
+            timeRemaining.Minutes,
+            timeRemaining.Seconds);
+        textMesh.text = timeRemainingString;
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        // If the app is being paused, save the end date to PlayerPrefs
+        if (pauseStatus)
         {
-            textMesh.text = originalText + "\nTime's up!";
+            PlayerPrefs.SetString("endDate", endDate.ToString());
         }
     }
 }
